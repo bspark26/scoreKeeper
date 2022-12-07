e23af0f9664f7d0b07df41a5d02acfc4cc3f9425
bspark26 <82602341+bspark26@users.noreply.github.com>
2022-12-07 05:55:26 +0000

    Added basic function for Player 1

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+const p1Digit = document.querySelector("#p1Digit");
+const p2Digit = document.querySelector("#p2Digit");
 
+let p1Score = 0;
+
+p1Button.addEventListener("click", function () {
+  p1Score += 1;
+  p2Digit.textContent = p1Score;
+});
